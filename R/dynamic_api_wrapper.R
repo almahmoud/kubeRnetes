33191326908b1217f0708d693560fb381e4e0ecb
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-09-18 13:46:24 -0400

    Add working wrappers

--- R/dynamic_api_wrapper.R
+++ R/dynamic_api_wrapper.R
@@ -1,3 +1,49 @@
+#' Generate API Wrapper Functions
+#'
+#' This function dynamically generates wrapper functions for all operations in the API.
+#'
+#' @param api The API object returned by rapiclient::get_api()
+#' @param operations A list of operations returned by rapiclient::get_operations()
+#' @return A list of generated wrapper functions
+#' @export
+generate_api_wrappers <- function(api, operations) {
+  wrappers <- list()
+  
+  for (op_name in names(operations)) {
+    wrappers[[op_name]] <- create_wrapper_function(api, operations, op_name)
+  }
+  
+  return(wrappers)
+}
+
+#' Create Wrapper Function
+#'
+#' This function creates a wrapper function for a specific API operation.
+#'
+#' @param api The API object returned by rapiclient::get_api()
+#' @param operations A list of operations returned by rapiclient::get_operations()
+#' @param op_name The name of the operation
+#' @return A wrapper function for the specified operation
+create_wrapper_function <- function(api, operations, op_name) {
+  op <- operations[[op_name]]
+  
+  wrapper <- function(...) {
+    tryCatch({
+      result <- op(...)
+      return(httr::content(result))
+    }, error = function(e) {
+      stop(paste("Error in", op_name, ":", conditionMessage(e)))
+    })
+  }
+  
+  # Set function name and add basic documentation
+  formals(wrapper) <- alist(...=)
+  attr(wrapper, "name") <- op_name
+  attr(wrapper, "description") <- paste("Wrapper for the", op_name, "API operation")
+  
+  return(wrapper)
+}
+
 #' Initialize API and Generate Operations Wrapper
 #'
 #' This function initializes the API and generates wrapper functions for all operations.
@@ -10,27 +56,26 @@
 initialize_api <- function(url, config = NULL, type = NULL) {
   api <- rapiclient::get_api(url, config, type)
   operations <- rapiclient::get_operations(api, .headers = config$headers)
+  wrappers <- generate_api_wrappers(api, operations)
   
-  return(list(api = api, operations = operations))
+  return(list(api = api, operations = operations, wrappers = wrappers))
 }
-
 
 #' List Available API Operations
 #'
 #' This function lists all available API operations and their basic information.
 #'
-#' @param api The API objected returned by initialize_api()
-#' @return A data frame of operation names
+#' @param api The API list object returned by initialize_api()
+#' @return A data frame of operation names and descriptions
 #' @export
 list_api_operations <- function(api) {
-  wrappers <- api_wrappers$wrappers
+  wrappers <- api$wrappers
   
   operations <- data.frame(
-    operation = names(operations)
+    operation = names(wrappers),
+    description = sapply(wrappers, attr, "description"),
+    stringsAsFactors = FALSE
   )
   
   return(operations)
 }
-
-
-
